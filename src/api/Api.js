5920c56d0370e3af30da0adafeeeb775325dbd48
Ayush Baranwal <abaran803@gmail.com>
2022-03-18 00:54:34 +0530

    Dummy FrontEnd Added

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,5 +1,35 @@
 const URL = 'http://localhost:8080';
 
-export const addPost = (post) => {
-    console.log(post);
+export const getAllPost = (cb) => {
+    fetch(`${URL}/showAllPosts`)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then((response) => {
+            cb(response)
+        })
+        .catch(err => cb(false));
+}
+
+export const addPost = (data, cb) => {
+    console.log('Adding Post')
+    fetch(`${URL}/newPost`, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        .then((response) => {
+            if (!response.ok) {
+                console.log('Error Occurred');
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            cb(true);
+            return response;
+        })
+        .catch(err => cb(false));
 }
--- src/components/AllCards.js
+++ src/components/AllCards.js
@@ -1,7 +1,9 @@
 import {Container, Row, Col} from "react-bootstrap";
 import CustomCard from "./CustomCard";
+import {getAllPost} from "../api/Api";
+import {useEffect, useState} from "react";
 
-const AllCards = () => {
+const AllCards = props => {
 
     const cardData = [
         {'title': 'Card Title1', 'description': "Some quick example text to build on the card title and make up the bulk of the card's content."},
@@ -24,9 +26,9 @@ const AllCards = () => {
     return (
         <Container fluid className='w-75'>
             <Row>
-                {cardData.map(item => (
-                    <Col key={item.title} lg={4} md={6} sm={12}><CustomCard card={item}/></Col>
-                ))}
+                {props.posts ? props.posts.map((item, index) => (
+                    <Col key={index} lg={4} md={6} sm={12}><CustomCard card={item}/></Col>
+                )) : <div>No post found!!!</div>}
             </Row>
         </Container>
     )
